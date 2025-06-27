17cc7bca4b543277ae36b985aa972c9fd9c99d98
LACHQER-Wiam <lachqerwiam@gmail.com>
2025-06-27 19:25:02 +0200

    First version fast API

--- code/app_FastAPI.py
+++ code/app_FastAPI.py
@@ -1,12 +1,23 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Path, HTTPException
+from main import generate_answers
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 
-@app.get("/get_answer_&_code/{file}{table_description}{user_input}")
-def get_answer_code(file, table_description: str, user_input: str) -> str:
-    if True:
+@app.get("/get_answer_code/{filename}/{table_description}/{user_input}/{analyzer}")
+def get_answer_code(filename: str, table_description: str, user_input: str, analyzer: bool) -> dict:
+    if 1==2:
         raise HTTPException(status_code = 404, detail="Error")
+    
+    code, answer, code_return = generate_answers(filename, 
+                                                 user_input, 
+                                                 table_description, 
+                                                 analyzer=analyzer)
+    
+    if analyzer==False:
+        code_return = code_return.to_dict(orient="records")
 
-
-    return {"code": code}
+    return JSONResponse(content={"code": code,
+            "answer": answer,
+            "code_return": code_return})
